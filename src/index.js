e727bc3af7c27241bd0bd820b344513a139bb354
Zach <zachary.tippit@gmail.com>
2022-09-16 13:42:47 -0600

    Beginning Redux Implementation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
