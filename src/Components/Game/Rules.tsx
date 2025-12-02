04f1dcdd0439df5354da72204fc3631b759f9f06
Zach Tippit <zachary.tippit@gmail.com>
2025-12-02 00:44:41 -0500

    Update Rules.tsx

--- src/Components/Game/Rules.tsx
+++ src/Components/Game/Rules.tsx
@@ -1,6 +1,12 @@
 import { useCurrentPuzzleAttempt } from "@/hooks/useCurrentPuzzleAttempt";
 import { useDailyPuzzle } from "@/hooks/useDailyPuzzle";
-import { getColumnRules, getColumnRuleStatuses, getRowRules, getRowRuleStatuses } from "@/utils/ruleUtils";
+import { cn } from "@/utils/cn";
+import {
+  getColumnRules,
+  getColumnRuleStatuses,
+  getRowRules,
+  getRowRuleStatuses,
+} from "@/utils/ruleUtils";
 
 const RulesRow = () => {
   const { data: dailyPuzzle } = useDailyPuzzle();
@@ -9,7 +15,7 @@ const RulesRow = () => {
   const rulesSource = currentAttempt?.progress ?? dailyPuzzle?.puzzle_array ?? [];
   const rules = getRowRules(rulesSource);
   const statuses = getRowRuleStatuses(rulesSource);
-  
+
   return (
     <div className="grid grid-rows-7 gap-y-0.5 mr-2">
       {rules.map((rule, r) => (
@@ -17,7 +23,10 @@ const RulesRow = () => {
           {rule.map((num, i) => (
             <span
               key={i}
-              className={`text-xs leading-none mr-1 ${statuses[r]?.[i]?.satisfied ? "line-through opacity-60" : ""}`}
+              className={cn(
+                "text-base font-bold leading-none mr-1",
+                statuses[r]?.[i]?.satisfied ? "line-through decoration-3" : ""
+              )}
             >
               {num}
             </span>
@@ -41,20 +50,21 @@ const RulesCol = () => {
       {rules.map((rule, c) => (
         <div key={c} className="flex flex-col items-center justify-end h-12 gap-y-2">
           {/* 👇 reverse order so top aligns with top of grid */}
-          {rule
-            .slice()
-            .map((num, i) => (
-              <span
-                key={i}
-                className={`text-xs leading-none ${statuses[c]?.[i]?.satisfied ? "line-through opacity-60" : ""}`}
-              >
-                {num}
-              </span>
-            ))}
+          {rule.slice().map((num, i) => (
+            <span
+              key={i}
+              className={cn(
+                "text-base leading-none font-bold",
+                statuses[c]?.[i]?.satisfied ? "line-through decoration-3" : ""
+              )}
+            >
+              {num}
+            </span>
+          ))}
         </div>
       ))}
     </div>
   );
 };
 
-export { RulesRow, RulesCol }
+export { RulesRow, RulesCol };
