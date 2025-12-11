f1fa3e29be9b825727737070054fe380c1160ce2
Zach Tippit <zachary.tippit@gmail.com>
2025-12-10 19:30:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,6 @@ import { PostHogProvider } from "posthog-js/react";
 import { UIProvider } from "./providers/UIProvider";
 import { AppRouterProvider } from "./router";
 
-// const analyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
-
-// if (analyticsId) {
-//   ReactGA.initialize(analyticsId);
-// } else if (import.meta.env.DEV) {
-//   // Surface helpful warning locally when analytics isn't configured.
-//   // eslint-disable-next-line no-console
-//   console.warn('Google Analytics ID is not set; analytics will be disabled.');
-// }
-
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: "2025-11-30",
